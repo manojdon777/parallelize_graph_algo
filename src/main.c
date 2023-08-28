1b82fec00ca997494942740552ead23bd6ff03ab
Manoj Dongare <manojdon777@gmail.com>
2023-08-28 14:01:31 +0530

    renamed make_tutorial to src

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
+#include "generategraph.h"
+#include "serialbfs.h"
 
-int int main(int argc, char* argv[])
-{
-	
-	
+
+int main(int argc, char* argv[]) {
+	int vertices = atoi(argv[1]);
+	int edges = atoi(argv[2]);
+	int **graph;
+	int i;
+	// Edge graph[edges];
+
+	graph = (int **)malloc(vertices * sizeof(int*));
+    for (i = 0; i < vertices; i++)
+        graph[i] = (int *)malloc(vertices * sizeof(int));
+
+	generateGraph(graph, vertices, edges);
+	// for (int i = 0; i < edges; i++) {
+	// 	printf("(%d, %d)\n", graph[i].x, graph[i].y);
+	// }
+	serial_bfs(graph, vertices);	
+	printf("\nAll working fine upto here");
+	printf("\n");
+	return 0;
 }
